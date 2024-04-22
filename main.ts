b499ef06f39b5383ebaf8a2567522eb66085554b
Hironori Wada <hironori104@gmail.com>
2024-04-22 22:21:14 +0900

    shuffle

--- main.ts
+++ main.ts
@@ -1,7 +1,46 @@
-import { cuvalCards, renderCli } from "./lib.ts";
+import { cuvalCards } from "./lib.ts";
+
+let cards = cuvalCards();
+// const view = renderCli(cuvalCards());
 
 Deno.serve((request) => {
-  request.url;
-  const view = renderCli(cuvalCards());
-  return new Response(`url is ${request.url}\n` + view);
+  const url = new URL(request.url);
+  const pathname = url.pathname;
+
+  if (pathname === "/reset" && request.method === "POST") {
+    cards = cuvalCards();
+    url.pathname = "/";
+    return Response.redirect(url.href);
+  }
+
+  for (let i = 0; i < 6; i++) {
+    if (pathname === `/${i + 1}`) {
+      return new Response(
+        `
+pathname is ${pathname}
+Your Card is ` + cards[i],
+      );
+    }
+  }
+
+  return new Response(
+    `
+    <html>
+    <body>
+    <a href="/1"> 1</a>
+    <a href="/2"> 2</a>
+    <a href="/3"> 3</a>
+    <a href="/4"> 4</a>
+    <a href="/5"> 5</a>
+    <a href="/6"> 6</a>
+    <form action="reset" method="post">
+      <button>shuffle</button>
+    </form>
+    </body>
+    </html>
+    `,
+    {
+      headers: { "content-type": "text/html" },
+    },
+  );
 });
