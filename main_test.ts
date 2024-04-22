21de307ce549234e9c8cd7198fd0c5944633a565
Hironori Wada <hironori104@gmail.com>
2024-04-22 21:15:04 +0900

    add test case

--- main_test.ts
+++ main_test.ts
@@ -1,4 +1,7 @@
-import { assertEquals } from "https://deno.land/std/assert/mod.ts";
+import {
+  assertEquals,
+  assertNotEquals,
+} from "https://deno.land/std/assert/mod.ts";
 import { cuvalCards } from "./main.ts";
 
 Deno.test("cuvalCards length", () => {
@@ -13,4 +16,10 @@ Deno.test("cuvalCards v", () => {
   assertEquals(cards.filter((c) => c === "prophet").length, 1);
   assertEquals(cards.filter((c) => c === "manwolf").length, 2);
   assertEquals(cards.filter((c) => c === "villeger").length, 3);
+});
+
+Deno.test("cuvalCards randomness", () => {
+  const res1 = cuvalCards();
+  const res2 = cuvalCards();
+  assertNotEquals(res1, res2);
 });
