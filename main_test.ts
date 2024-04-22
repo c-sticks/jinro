42f5e2736f58d23ba833f46288ea23018082dddd
YuseiSato0302 <2123065@ed.tus.ac.jp>
2024-04-22 20:43:01 +0900

    add test case

--- main_test.ts
+++ main_test.ts
@@ -7,3 +7,18 @@ if (cards.length === 6) {
 } else {
   console.log("Test failed");
 }
+
+let sum_prophet = 0;
+for (const card of cards) {
+  if (
+    card === "prophet"
+  ) {
+    sum_prophet++;
+  }
+}
+
+if (sum_prophet === 1) {
+  console.log("Test passed");
+} else {
+  console.log("Test failed");
+}
