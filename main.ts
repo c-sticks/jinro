9a5a4c69d21ebefe947958fcb68e25ed8618f565
Hironori Wada <hironori104@gmail.com>
2024-04-22 22:10:49 +0900

    wip

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 import { cuvalCards, renderCli } from "./lib.ts";
 
 Deno.serve((request) => {
+  request.url;
   const view = renderCli(cuvalCards());
-  return new Response(view);
+  return new Response(`url is ${request.url}\n` + view);
 });
-
-// aaa
